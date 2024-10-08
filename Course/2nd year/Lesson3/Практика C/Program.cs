34c53d3564380a77306d2fdacf0aa8dafae89fae
Ivan <ivanmolodyh33@gmail.com>
2024-10-08 20:55:13 +0400

    практика С

--- Course/2nd year/Lesson3/Практика C/Program.cs
+++ Course/2nd year/Lesson3/Практика C/Program.cs
@@ -10,6 +10,16 @@ namespace Практика_C
     {
         static void Main(string[] args)
         {
+            int[] emas = new int[4];
+            int[] masnum = new int[] { 1, 2, 3, 4, 5 };
+            char[] massim = { 'a', 'b', 'c' };
+            int[] masspen = new int[5];
+            masspen[0] = 1;
+            masspen[1] = 2;
+            masspen[2] = 3;
+            masspen[3] = 4;
+            masspen[4] = 5;
+            Console.ReadKey();
         }
     }
 }
