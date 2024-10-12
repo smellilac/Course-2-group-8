e5a736be4c387692e82b206b16cb7d3a9c58b833
Ivan <ivanmolodyh33@gmail.com>
2024-10-12 12:24:12 +0400

    Домашняя работа задание номер 1

---
+++ Course/2nd year/Lesson2/Домашняя работа/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашняя_работа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double sum = num1 + num2;
+            Console.WriteLine("Сумма чисел:" + sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
