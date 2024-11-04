3325b2e360c5ae6b68e3fc26f4db36bb76a734be
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 11:15:21 +0400

    Задачи

---
+++ Course/2nd year/Lesson4/Задача под номером 9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_под_номером_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 5, 6, 4, 3, 7, 2, 8, 10, 9 };
+            Array.Sort(num);
+            for (int i=0; i < num.Length; i++) 
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
