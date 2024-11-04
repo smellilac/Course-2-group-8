1ce7d76bcef10adbb8ecdb24243b5a68a2159a44
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 17:30:21 +0400

    Практика А

---
+++ Course/2nd year/Lesson6/PracticeA/8. Функция Расчета Факториала/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Функция_Расчета_Факториала
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int num = int.Parse(Console.ReadLine());
+            int factorial = Cosmos(num);
+            Console.WriteLine("Факториал:" + num + ",равен:" + factorial);
+            Console.ReadKey();
+        }
+        public static int Cosmos(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n*Cosmos(n-1);
+            }
+        }
+
+    }
+}
