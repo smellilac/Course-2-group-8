24ac0ffdebafa5f672022ff97f6bdc3cdeed0092
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 16:39:57 +0400

    Практика А

---
+++ Course/2nd year/Lesson6/PracticeA/6. Функция Обратной Строки/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Функция_Обратной_Строки
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string STR = Console.ReadLine();
+            string reversed = Reverse(STR);
+            Console.WriteLine("Обратная строка:"+reversed);
+            Console.ReadKey();
+        }
+        public static string Reverse(string str)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+}
