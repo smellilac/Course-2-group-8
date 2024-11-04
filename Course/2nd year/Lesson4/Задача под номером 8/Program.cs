365630f943cb07d16c36528708e8cca880a60442
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 11:03:15 +0400

    Задачи

---
+++ Course/2nd year/Lesson4/Задача под номером 8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_под_номером_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            int min = num[0];
+            int max = num[0];
+            for (int i = 1; i < num.Length; i++) 
+            {
+                if (num[i] < min)
+                {
+                    min=num[i];
+                }
+                if (num[i] > max) 
+                {
+                    max=num[i];
+                }
+            }
+            Console.WriteLine("Минимальное значение:" + min);
+            Console.WriteLine("Максимальное значение:" + max);
+            Console.ReadKey();
+        }
+    }
+}
