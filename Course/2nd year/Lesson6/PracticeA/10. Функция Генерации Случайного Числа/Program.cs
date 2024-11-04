c733208f4ce9135bccae815c8f8ab3a78ea0fea1
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 18:11:07 +0400

    Прктика А

---
+++ Course/2nd year/Lesson6/PracticeA/10. Функция Генерации Случайного Числа/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Функция_Генерации_Случайного_Числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введит наименьшее число:");
+            int lower = int.Parse(Console.ReadLine());
+            Console.Write("Введите наибольшее число:");
+            int upper = int.Parse(Console.ReadLine());
+
+        
+            int randomNumber =Number(lower, upper);
+
+
+            Console.WriteLine("Случайное число в диапазоне от:" + lower + ",до" + upper + ":" + randomNumber);
+            Console.ReadKey();
+        }
+
+        
+        public static int Number(int lower, int upper)
+        {
+            Random random = new Random();
+            return random.Next(lower, upper + 1);
+        }
+    }
+    
+}
---
+++ Course/2nd year/Lesson6/PracticeA/10. Функция Генерации Случайного Числа/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("10. Функция Генерации Случайного Числа")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10. Функция Генерации Случайного Числа")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("65021d04-4030-4a53-b89e-f5478c1cdc23")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
