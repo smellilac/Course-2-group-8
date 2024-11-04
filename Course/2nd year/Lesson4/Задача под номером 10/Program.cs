6f67d9935e72daf0063d962ebcf753b25d749b2b
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 11:28:56 +0400

    Задачи

---
+++ Course/2nd year/Lesson4/Задача под номером 10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_под_номером_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, -1, -2, -3, -4, 4, 5, 6, 7 };
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] < 1)
+                {
+                    num[i] = 0;
+                }
+            }
+            Console.WriteLine("Изменённый массив:");
+            foreach (int E in num)
+            {
+                Console.WriteLine(E + "");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Course/2nd year/Lesson4/Задача под номером 10/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Задача под номером 10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Задача под номером 10")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("466d9a42-4af4-4af6-8dfd-6e5996d9c304")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
