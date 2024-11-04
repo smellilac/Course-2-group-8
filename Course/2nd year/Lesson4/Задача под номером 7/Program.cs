e4ef6e004817643cc900803acc02b9602c08bd41
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 10:50:38 +0400

    Задачи

---
+++ Course/2nd year/Lesson4/Задача под номером 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_под_номером_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12 };
+            Console.WriteLine("Элементы на нечётных позициях:");
+            for (int i = 0; i < num.Length; i+=2)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
