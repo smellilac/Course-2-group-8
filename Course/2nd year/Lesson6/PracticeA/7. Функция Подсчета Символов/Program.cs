84c153a3e966fb31441d3a66483367a12bef4714
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 17:15:44 +0400

    Практика А

---
+++ Course/2nd year/Lesson6/PracticeA/7. Функция Подсчета Символов/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Функция_Подсчета_Символов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свою строку:");
+            string slova = Console.ReadLine();
+            Console.WriteLine("Введи определённый символ:");
+            char target = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            int count = CountChaR(slova, target);
+            Console.WriteLine("Символ:" + target + ",встречается:" + count + "раз в строке.");
+            Console.ReadKey();
+        }
+
+        public static int CountChaR(string str, char target)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == target)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Course/2nd year/Lesson6/PracticeA/7. Функция Подсчета Символов/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("7. Функция Подсчета Символов")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("7. Функция Подсчета Символов")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b74f402b-28ca-4f49-827a-d6638c5d078e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
