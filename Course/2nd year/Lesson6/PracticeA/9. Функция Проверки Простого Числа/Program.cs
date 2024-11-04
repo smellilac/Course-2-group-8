ca468e9194d5cbb4dd825ee093e95374f4a170a0
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 17:53:06 +0400

    Практика А

---
+++ Course/2nd year/Lesson6/PracticeA/9. Функция Проверки Простого Числа/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Функция_Проверки_Простого_Числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int num = int.Parse(Console.ReadLine());
+            bool FL = F(num);
+            if (FL)
+            {
+                Console.WriteLine(num + "-простое число");
+            }
+            else
+            {
+                Console.WriteLine(num + "-непростое число");
+            }
+            Console.ReadKey();
+        }
+        public static bool F(int num)
+        {
+            if (num <= 0)
+            {
+                return false;
+            }
+                return true;
+           
+        }
+    }
+}
