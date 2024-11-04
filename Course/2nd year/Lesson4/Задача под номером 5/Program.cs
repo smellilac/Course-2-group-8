73adcd17df4cd4bc9ca2ca938d50b74400b5b2ad
Ivan <ivanmolodyh33@gmail.com>
2024-11-04 10:16:37 +0400

    Задачи

---
+++ Course/2nd year/Lesson4/Задача под номером 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_под_номером_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = {-1, 1, -3, 5, -6, -7, 10,-12};
+            int negcount = 0;
+            foreach (int numb in num) 
+            {
+                if (numb < 0) 
+                {
+                    negcount++;
+                }
+            }
+            Console.WriteLine("Количество отрицательных чисел:" + negcount);
+            Console.ReadKey();
+        }
+    }
+}
